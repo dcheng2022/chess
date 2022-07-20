7b11d54535314422ddc6f6a70ddba431dae8bb48
Darin <darinchengg@gmail.com>
2022-07-20 15:44:09 -0500

    Add classes for every piece and make descendant of Piece class

--- lib/chess.rb
+++ lib/chess.rb
@@ -86,3 +86,21 @@ class Piece
     @pso = pos
   end
 end
+
+class Pawn < Piece
+end
+
+class Rook < Piece
+end
+
+class Knight < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+end
